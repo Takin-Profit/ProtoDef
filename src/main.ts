861b90f7e7c4d0b0971a39a39a6172ae890eb1c3
g5becks <techstar.dev@hotmail.com>
2023-07-12 13:25:56 -0400

    feat: start work on parsing files
    getting an overall feel for the asvs lib and parsing

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+// Copyright 2023 Takin Profit. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import asvc from 'avsc'
+import path from 'node:path'
+import url from 'node:url'
+
+const makePath = (file: string) =>
+  path.join(
+    path.dirname(url.fileURLToPath(import.meta.url).replace('/src', '')),
+    'api',
+    file
+  )
+
+/**
+  const getProtocol = (file: string) => {
+    return readFileSync(makePath(file), 'utf8')
+  }
+*/
+
+asvc.assembleProtocol(makePath('pt.avdl'), (err, schema) => {
+  console.log(schema)
+  console.error(err)
+})
---
+++ src/util.ts
@@ -0,0 +1,30 @@
+// Copyright 2023 Takin Profit. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+export type Lang = 'ts' | 'py'
+export const hasDoc = (schema: object) =>
+  Object.prototype.hasOwnProperty.call(schema, 'doc')
+
+export const hasNameSpace = (schema: object) =>
+  Object.prototype.hasOwnProperty.call(schema, 'namespace')
+
+const protocol = 'protocol'
+
+// convert CamelCase to snake_case
+export const camelToSnake = (str: string) => {
+  return str.replaceAll(/[A-Z]/g, letter => `_${letter.toLowerCase()}`)
+}
+
+export const protoName = (schema: Record<string, unknown>): string =>
+  schema[protocol] as string
+
+export const makeFileName = (
+  schema: Record<string, unknown>,
+  lang: Lang = 'ts',
+  fileNameMaker: (lang: Lang, fileName: string) => string = (lang, fileName) =>
+    fileName
+) =>
+  lang === 'ts' || lang === 'py'
+    ? `${camelToSnake(protoName(schema))}.py`
+    : fileNameMaker(lang, protoName(schema))
